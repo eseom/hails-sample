64b81e08937c04a0856126f4ddb98e37b5fa9c54
eseom <me@eseom.org>
2017-06-01 15:49:51 +0900

    hails 0.1.14

--- src/server/user/model.js
+++ src/server/user/model.js
@@ -1,16 +1,18 @@
-export default (sequelize, DataTypes) => {
-  const { INTEGER, DATE, STRING } = DataTypes
-  const User = sequelize.define('users', {
-    id: { type: INTEGER, primaryKey: true, autoIncrement: true, scopes: ['public'] },
-    username: { type: STRING },
-    createdAt: { field: 'created_at', type: DATE },
-    updatedAt: { field: 'updated_at', type: DATE },
-  },
-    {
-      createdAt: 'createdAt',
-      updatedAt: 'updatedAt',
-    })
-  return {
-    User,
-  }
+import { server } from 'hails'
+
+const { sequelize, DataTypes: { INTEGER, DATE, STRING } } = server
+
+const User = sequelize.define('users', {
+  id: { type: INTEGER, primaryKey: true, autoIncrement: true, scopes: ['public'] },
+  username: { type: STRING },
+  createdAt: { field: 'created_at', type: DATE },
+  updatedAt: { field: 'updated_at', type: DATE },
+},
+{
+  createdAt: 'createdAt',
+  updatedAt: 'updatedAt',
+})
+export {
+  User,
 }
+
